0f2e06308428582a21143d560eb961ae9be29544
vitoria2002campos <vitoriadelara45@gmail.com>
2022-02-17 01:59:53 -0300

    implementacoes

--- urna/script.js
+++ urna/script.js
@@ -1,22 +1,38 @@
 let seuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
-let descricaoGeral = document.querySelector('.d-1-4');
+let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
+let numero ='';
 
-function comecarEtapa(){
+
+function comecarEtapa() {
     let etapa = etapas[etapaAtual];
     let numeroHtml = '';
+    
+    for (let i = 0; i<etapa.numeros; i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+        }else{
 
+            numeroHtml += '<div class="numero"></div>';
+        }
+        
+        
+    }
     seuVotoPara.style.display = 'none';
     cargo.innerHTML = etapa.titulo;
     descricao.innerHTML = '';
     aviso.style.display = 'none';
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    
 }
 
 function clicou(n){
